75805dc46f731ffe17ecc48bf427aa223a315542
w <wangchonghuan@gmail.com>
2023-06-16 13:16:48 +0100

    poststart log and config

--- src/configmap/master-poststart.sh
+++ src/configmap/master-poststart.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -e
+# Check if the directory exists, if not, create it
+if [ ! -d "/spok_logs" ]; then
+  mkdir -p /spok_logs
+fi
+LOG_PATH="/spok_logs/poststart.log"
+echo "SYNC_COMMIT value: ${SYNC_COMMIT}" >> ${LOG_PATH}
+
 # Poll until Postgres is ready
 until pg_isready -U postgres; do
-    echo "Waiting for PostgreSQL to start..."
+    echo "Waiting for PostgreSQL to start..." >> ${LOG_PATH}
     sleep 2
 done
-echo "Post-start setup as Master..."
+echo "Post-start setup as Master..." >> ${LOG_PATH}
+
 # Perform master specific setup
 psql -U postgres -c "ALTER SYSTEM SET listen_addresses TO '*';"
 psql -U postgres -c "ALTER SYSTEM SET wal_level TO replica;"
@@ -18,8 +26,9 @@ psql -U postgres -c "SELECT pg_reload_conf();"
 psql -U postgres -c "SELECT * FROM pg_create_physical_replication_slot('pgset1_slot');"
 
 # Check if SYNC_COMMIT is true
-#if [ "${1}" = "true" ]; then
+if [ "${SYNC_COMMIT}" = "true" ]; then
+    echo "Set sync configs" >> ${LOG_PATH}
     psql -U postgres -c "ALTER SYSTEM SET synchronous_standby_names TO '*';"
     psql -U postgres -c "ALTER SYSTEM SET synchronous_commit TO on;"
     psql -U postgres -c "SELECT pg_reload_conf();"
-#fi
+fi
--- src/configmap/replica-poststart.sh
+++ src/configmap/replica-poststart.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 set -e
+
+# Check if the directory exists, if not, create it
+if [ ! -d "/spok_logs" ]; then
+  mkdir -p /spok_logs
+fi
+
+LOG_PATH="/spok_logs/poststart.log"
+echo "SYNC_COMMIT value: ${SYNC_COMMIT}" >> ${LOG_PATH}
+
 # Poll until Postgres is ready
 until pg_isready -U postgres; do
-    echo "Waiting for PostgreSQL to start..."
+    echo "Waiting for PostgreSQL to start..." >> ${LOG_PATH}
     sleep 2
 done
-echo "Post-start setup as Replica..."
+echo "Post-start setup as Replica..." >> ${LOG_PATH}
+
 # Perform replica specific setup
 touch /var/lib/postgresql/data/pgdata/standby.signal
 
@@ -13,9 +23,10 @@ touch /var/lib/postgresql/data/pgdata/standby.signal
 PRIMARY_CONNINFO="user=replicarole password=SuperSecret host=pgset-master-0.pgsql-headless.default.svc.cluster.local port=5432 sslmode=prefer sslcompression=0 gssencmode=prefer krbsrvname=postgres target_session_attrs=any"
 
 # Check if SYNC_COMMIT is true and append application_name if necessary
-#if [ "${1}" = "true" ]; then
+if [ "${SYNC_COMMIT}" = "true" ]; then
     PRIMARY_CONNINFO+=" application_name=pgset-replica-0.pgsql-headless.default.svc.cluster.local"
-#fi
+    echo "SYNC_COMMIT is true, appended application_name" >> ${LOG_PATH}
+fi
 
 # Write primary_conninfo to the configuration
 echo "primary_conninfo = '${PRIMARY_CONNINFO}'" >> /var/lib/postgresql/data/pgdata/postgresql.auto.conf
